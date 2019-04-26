a9f96d7219e805edcf3d3ac3940334cbd86c8ead
larry <yangxiaonan@vagelos-ve502-0123.apn.wlan.upenn.edu>
2019-04-26 17:31:38 -0400

    item_parse

--- amazonCrawler/amazonCrawler/spiders/clothing.py
+++ amazonCrawler/amazonCrawler/spiders/clothing.py
@@ -8,11 +8,9 @@ class ClothingSpider(scrapy.Spider):
     start_urls = ['https://www.amazon.com/gp/product/B07HN3ZCS1/ref=s9_acsd_newrz_hd_bw_b2hbDCl_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-11&pf_rd_r=2N3V3PDNRKF16P1NF9W7&pf_rd_t=101&pf_rd_p=e1e9ee3d-e9cf-5b00-b9b2-4c2862506279&pf_rd_i=2476517011']
 
     def parse(self, response):
-    	# title
-        for title in response.css('#productTitle::text').extract():
-            print(title.strip())
-        # brand
-        response.css('#brand').attrib['href'].split("/")[1]
-
-        # for next_page in response.css('a.next-posts-link'):
-        #     yield response.follow(next_page, self.parse)
+    	title = response.css('#productTitle::text').extract_first().strip()
+    	brand = response.css('#brand').attrib['href'].split("/")[1]
+    	price = response.css('#priceblock_ourprice::text').extract()[0]
+    	rate = response.css('span.a-icon-alt::text').extract_first().split(" ")[0]
+    	category = response.xpath("//div[@id='wayfinding-breadcrumbs_feature_div']/ul/li/span[@class='a-list-item']/a/text()").extract()[-1].strip()
+    	features = [x.strip() for x in response.xpath("//div[@id='feature-bullets']/ul/li/span[@class='a-list-item']/text()").extract()]
