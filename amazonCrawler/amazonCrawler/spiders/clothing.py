0eb2f6a1b6f54d21f7c02cec6363c40c86626251
HanyuSylvieSun <sunhanyu@seas.upenn.edu>
2019-04-26 21:32:25 +0000

    commit before merge

--- amazonCrawler/amazonCrawler/spiders/clothing.py
+++ amazonCrawler/amazonCrawler/spiders/clothing.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 class ClothingSpider(scrapy.Spider):
     name = 'clothing'
     allowed_domains = ['www.amazon.com']
-    start_urls = ['https://www.amazon.com/gp/product/B07HN3ZCS1/ref=s9_acsd_newrz_hd_bw_b2hbDCl_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-11&pf_rd_r=2N3V3PDNRKF16P1NF9W7&pf_rd_t=101&pf_rd_p=e1e9ee3d-e9cf-5b00-b9b2-4c2862506279&pf_rd_i=2476517011']
+    start_urls = ['https://www.amazon.com/s/ref=lp_1040660_ex_n_3?rh=n%3A7141123011%2Cn%3A7147440011%2Cn%3A1040660%2Cn%3A1045024&bbn=1040660&ie=UTF8&lo=fashion']
+    
+    # Get link for each items and query
+    def parse(self,response):
+        for item in response.css('div.s-item-container'):
+            link = item.css('a.a-link-normal.a-text-normal ::attr(href)').extract()[0]
+            print(link)
+            scrapy.Request(link, callback=self.parse_item)     
 
-    def parse(self, response):
-    	# title
+    def parse_item(self, response):
+        print("HERE")
         for title in response.css('#productTitle::text').extract():
+            print("HERE")
             print(title.strip())
+            yield title.strip()
+        return;
         # brand
-        response.css('#brand').attrib['href'].split("/")[1]
+        #response.css('#brand').attrib['href'].split("/")[1]
 
         # for next_page in response.css('a.next-posts-link'):
         #     yield response.follow(next_page, self.parse)
