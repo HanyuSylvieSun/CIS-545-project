df7c3bb30e99b4e753f57cad54645efb4b110ae8
HanyuSylvieSun <sunhanyu@seas.upenn.edu>
2019-04-28 05:33:05 +0000

    csv

--- amazonCrawler/amazonCrawler/spiders/clothing.py
+++ amazonCrawler/amazonCrawler/spiders/clothing.py
@@ -11,11 +11,11 @@ class ClothingSpider(scrapy.Spider):
     with open('men.csv', 'r') as f:
 		reader = csv.reader(f)
     	for row in reader:
-        	start_urls.add(row.split(",")[1])
+        	start_urls.append(row.split(",")[1])
     with open('women.csv', 'r') as f:
 		reader = csv.reader(f)
     	for row in reader:
-        	start_urls.add(row.split(",")[1])
+        	start_urls.append(row.split(",")[1])
     
     # Get link for each items and query
     def parse(self,response):
@@ -25,15 +25,6 @@ class ClothingSpider(scrapy.Spider):
             yield scrapy.Request(link, callback=self.parse_item)     
 
     def parse_item(self, response):
-<<<<<<< HEAD
-        title = response.css('#productTitle::text').extract_first().strip()
-        brand = response.css('#brand').attrib['href'].split("/")[1]
-        price = response.css('#priceblock_ourprice::text').extract()[0]
-        rate = response.css('span.a-icon-alt::text').extract_first().split(" ")[0]
-        category = response.xpath("//div[@id='wayfinding-breadcrumbs_feature_div']/ul/li/span[@class='a-list-item']/a/text()").extract()[-1].strip()
-        features = [x.strip() for x in response.xpath("//div[@id='feature-bullets']/ul/li/span[@class='a-list-item']/text()").extract()]
-
-=======
     	# brand = response.css('#brand').attrib['href'].split("/")[1]
     	brand = response.xpath("//div[@id='bylineInfo_feature_div']/div[@class='a-section a-spacing-none']/a/@href").extract()[0].split("/")[1]
     	title = response.css('#productTitle::text').extract_first().strip()
@@ -44,4 +35,3 @@ class ClothingSpider(scrapy.Spider):
     	colors = [x.split(" ")[-1] for x in response.xpath("//div[@id='variation_color_name']/ul/li/@title").extract()]
 		with open('items.txt', 'w') as f:
     		f.write(brand+','+title+','+price+','+rate+','+category+','+str(features) + ',' + str(colors))
->>>>>>> 2c7e589879ba0db94706ba729130038ccdf9ac76
