12cd836dc995509293878f58ac1decfcc8fe8530
larry <yangxiaonan@vagelos-ve502-0997.apn.wlan.upenn.edu>
2019-04-28 14:00:01 -0400

    IO

--- amazonCrawler/amazonCrawler/spiders/clothing.py
+++ amazonCrawler/amazonCrawler/spiders/clothing.py
@@ -7,15 +7,15 @@ import csv
 class ClothingSpider(scrapy.Spider):
     name = 'clothing'
     allowed_domains = ['www.amazon.com']
-    start_urls = []
-    with open('../../../category_to_link_men.txt', 'r') as f:
-    	for row in f:
-    		print(row)
-    		start_urls.append(row)
-    with open('../../../category_to_link_men.txt', 'r') as f:
-    	for row in f:
-    		print(row)
-    		start_urls.append(row)
+    start_urls = ["https://www.amazon.com/Mens-Shirts/b?ie=UTF8&node=2476517011"]
+    # with open('../../../category_to_link_men.txt', 'r') as f:
+    # 	for row in f:
+    # 		print(row)
+    # 		start_urls.append(row)
+    # with open('../../../category_to_link_men.txt', 'r') as f:
+    # 	for row in f:
+    # 		print(row)
+    # 		start_urls.append(row)
     
     # Get link for each items and query
     def parse(self,response):
@@ -33,5 +33,5 @@ class ClothingSpider(scrapy.Spider):
     	category = response.xpath("//div[@id='wayfinding-breadcrumbs_feature_div']/ul/li/span[@class='a-list-item']/a/text()").extract()[-1].strip()
     	features = [x.strip() for x in response.xpath("//div[@id='feature-bullets']/ul/li/span[@class='a-list-item']/text()").extract()]
     	colors = [x.split(" ")[-1] for x in response.xpath("//div[@id='variation_color_name']/ul/li/@title").extract()]
-    	with open('items.txt', 'w') as f:
+    	with open('items.txt', 'a', newline='\n') as f:
     		f.write(brand+','+title+','+price+','+rate+','+category+','+str(features) + ',' + str(colors))
